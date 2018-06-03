1485769af7059d5da98478465fbfabe58aa00e6f
Filiphe Vilar Figueiredo <liphvf@gmail.com>
2018-06-03 15:12:16 -0300

    +++

--- Genius/routes/index.js
+++ Genius/routes/index.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var mqtt = require('mqtt')
+
+var client = mqtt.connect('mqtt://hkxmleqo:8jrTUwSKMNJz@m10.cloudmqtt.com:13044')
+
+client.on('connect', function () {
+  console.log("conectado: ", client.connected);
+  client.subscribe('test')
+  // Caso queira enviar uma menssagem para exemplificar.
+  // client.publish('presence', 'Hello mqtt')
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  // client.end()
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,10 +24,13 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-router.get('/enviar', function(req, res, next) {
+router.post('/enviar/', function(req, res, next) {
   
-  console.log("Chegou aqui");
-  res.redirect('/');
+  console.log(req.body.resposta);
+  // client.publish('resposta', req.query.resposta);
+  
+  // res.redirect('/');
+  res.sendStatus(200);
 });
 
 module.exports = router;
